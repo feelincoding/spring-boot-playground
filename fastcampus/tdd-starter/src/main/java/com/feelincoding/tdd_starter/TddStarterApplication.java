0505bc99f2feb9e9cebc021b090199fbba5e9246
feelincoding <feelincoding@gmail.com>
2025-01-02 17:03:55 +0900

    tdd-starter start, day 1.

---
+++ fastcampus/tdd-starter/src/main/java/com/feelincoding/tdd_starter/TddStarterApplication.java
@@ -0,0 +1,13 @@
+package com.feelincoding.tdd_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddStarterApplication.class, args);
+	}
+
+}
