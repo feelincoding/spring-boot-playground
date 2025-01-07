42ad22bebafa3c2b023ab0cad9386fbb4e065a75
feelincoding <feelincoding@gmail.com>
2025-01-07 15:22:47 +0900

    도메인 추가: 게시글, 댓글

--- fastcampus/community-demo/src/main/java/com/feelincoding/community_demo/domain/ArticleComment.java
+++ fastcampus/community-demo/src/main/java/com/feelincoding/community_demo/domain/ArticleComment.java
@@ -3,12 +3,12 @@ package com.feelincoding.community_demo.domain;
 import java.time.LocalDateTime;
 
 public class ArticleComment {
-    private Long id;
-    private Article article;
-    private String content;
+    private Long id; 
+    private Article article; // 게시글 (ID)
+    private String content; // 본문
     
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
-    private String createdBy;
-    private String modifiedBy;
+    private LocalDateTime createdAt; // 생성일시
+    private LocalDateTime modifiedAt; // 수정일시
+    private String createdBy; // 생성자
+    private String modifiedBy; // 수정자
 }
